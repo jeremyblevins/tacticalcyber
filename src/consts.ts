251e2888ed1b7a4e3efd343cdde8e43a551ae244
jeremyblevins <jeremy@jeremyblevins.com>
2026-04-08 10:19:17 -0500

    Update consts.ts
    Added company name and site description.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Tactical Cyber, LLC";
+export const SITE_DESCRIPTION = "Tactical Cyber delivers managed IT and cybersecurity services that strengthen defenses, reduce risk, and keep businesses operational.";
